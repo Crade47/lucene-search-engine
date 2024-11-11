776555a6e6e5151ef799d3a423ea566c9731fbee
Dylan <115677483+aki415@users.noreply.github.com>
2024-11-11 10:04:18 +0000

    Create Constants.java

---
+++ twentythree/src/main/java/com/twenty_three/app/Constants.java
@@ -0,0 +1,13 @@
+package com.twenty_three.app;
+
+public class Constants {
+    public static final String docn = "DOC";
+    public static final String docnon = "DOCNO";
+    public static final String headlinen = "HEADLINE";
+    public static final String textn = "TEXT";
+    public static final String bylinen = "BYLINE";
+    public static final String pubn = "PUB";
+    public static final String daten = "DATE";
+    public static final String profilen = "PROFILE";
+    public static final String documentpath = "path-to-corpus";
+}
